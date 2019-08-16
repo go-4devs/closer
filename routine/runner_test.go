25340a709baeedfd4be263f9b4063188a1b07770
andrey1s <andrey@4devs.pro>
2019-08-16 21:35:10 +0300

    fix race in test

--- routine/runner_test.go
+++ routine/runner_test.go
@@ -1,6 +1,7 @@
 package routine
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -9,22 +10,29 @@ import (
 
 func TestGo(t *testing.T) {
 	var run int
-
+	var mu sync.Mutex
 	fnc := func() {
 		time.Sleep(time.Millisecond)
+		mu.Lock()
 		run++
+		mu.Unlock()
 	}
 	Go(fnc, fnc)
+	mu.Lock()
 	require.Equal(t, 0, run)
+	mu.Unlock()
 	Wait()
 	require.Equal(t, 2, run)
 }
 
 func TestWaitGroup_Close(t *testing.T) {
 	var run int
+	var mu sync.Mutex
 	fnc := func() {
 		time.Sleep(time.Millisecond)
+		mu.Lock()
 		run++
+		mu.Unlock()
 	}
 	wg := WaitGroup{}
 	wg.Go(fnc, fnc)
