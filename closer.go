f7ec9a416e988103aada636e90e5fce05f20dacf
andrey <andrey@4devs.org>
2019-08-21 11:00:08 +0300

    add gloabl closer by priority (#2)

--- closer.go
+++ closer.go
@@ -6,9 +6,15 @@ import (
 	"os"
 )
 
-//Closer interface
+// Closer interface
 type Closer interface {
 	Add(f ...func() error)
 	Wait(ctx context.Context, s ...os.Signal)
 	io.Closer
+}
+
+// ByPriority close by priority
+type ByPriority interface {
+	Closer
+	AddByPriority(priority uint8, f ...func() error)
 }
